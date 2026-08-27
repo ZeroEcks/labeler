f9bff66f85335509cbed7da83f3c1fc457b792bd
Melody Kelly <melody.kelly@qut.edu.au>
2026-08-27 14:53:05 +1000

    fix: adjust container build to go straight from cloudron

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,22 @@
-{ pkgs, lib, config, inputs, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
 
 let
+  inherit (lib) fileset hasPrefix;
   rustBuildDeps = with pkgs; [
     pkg-config
     openssl.dev
-    fontconfig freetype graphite2 icu libpng zlib
+    fontconfig
+    freetype
+    graphite2
+    icu
+    libpng
+    zlib
   ];
 
   # Fixes for tectonic crates because of their native library usage
@@ -35,58 +47,98 @@ let
     postConfigure = lib.concatMapStringsSep "\n" (o: (o attrs).postConfigure or "") overrides;
   };
 
-
   crateOverrides = pkgs.defaultCrateOverrides // {
-    tectonic_bridge_icu = attrs: {
-      nativeBuildInputs = [ pkgs.pkg-config ];
-      buildInputs = [ pkgs.icu ];
-      # icu needs the `.out` not `.dev`
-      postInstall = ''
-        if [ -f "$lib/lib/link" ]; then
-          sed -i "s#${pkgs.icu.dev}#${pkgs.icu.out}#g" "$lib/lib/link"
-        fi
-      '';
-    } // renameLinksVar "ICUUC" attrs;
-    tectonic_bridge_graphite2 = attrs: {
-      nativeBuildInputs = [ pkgs.pkg-config ];
-      buildInputs = [ pkgs.graphite2 ];
-    } // renameLinksVar "GRAPHITE2" attrs;
-    tectonic_bridge_png = attrs: {
-      nativeBuildInputs = [ pkgs.pkg-config ];
-      buildInputs = [ pkgs.libpng pkgs.zlib ];
-    } // renameLinksVar "PNG" attrs;
-    tectonic_bridge_freetype2 = attrs: {
-      nativeBuildInputs = [ pkgs.pkg-config ];
-      buildInputs = [ pkgs.freetype ];
-    } // renameLinksVar "FREETYPE2" attrs;
-    tectonic_bridge_fontconfig = attrs: {
-      nativeBuildInputs = [ pkgs.pkg-config ];
-      buildInputs = [ pkgs.fontconfig pkgs.freetype ];
-    } // renameLinksVar "FONTCONFIG" attrs;
-    tectonic_bridge_harfbuzz = mergePostConfigure [ (renameLinksVar "HARFBUZZ") fixupOutDirPath ];
+    tectonic_bridge_icu =
+      attrs:
+      {
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        buildInputs = [ pkgs.icu ];
+        # icu needs the `.out` not `.dev`
+        postInstall = ''
+          if [ -f "$lib/lib/link" ]; then
+            sed -i "s#${pkgs.icu.dev}#${pkgs.icu.out}#g" "$lib/lib/link"
+          fi
+        '';
+      }
+      // renameLinksVar "ICUUC" attrs;
+    tectonic_bridge_graphite2 =
+      attrs:
+      {
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        buildInputs = [ pkgs.graphite2 ];
+      }
+      // renameLinksVar "GRAPHITE2" attrs;
+    tectonic_bridge_png =
+      attrs:
+      {
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        buildInputs = [
+          pkgs.libpng
+          pkgs.zlib
+        ];
+      }
+      // renameLinksVar "PNG" attrs;
+    tectonic_bridge_freetype2 =
+      attrs:
+      {
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        buildInputs = [ pkgs.freetype ];
+      }
+      // renameLinksVar "FREETYPE2" attrs;
+    tectonic_bridge_fontconfig =
+      attrs:
+      {
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        buildInputs = [
+          pkgs.fontconfig
+          pkgs.freetype
+        ];
+      }
+      // renameLinksVar "FONTCONFIG" attrs;
+    tectonic_bridge_harfbuzz = mergePostConfigure [
+      (renameLinksVar "HARFBUZZ")
+      fixupOutDirPath
+    ];
     tectonic_xetex_layout = fixupOutDirPath;
     tectonic_bridge_core = relocateManifestSubdir "support";
     tectonic_bridge_flate = relocateManifestSubdir "include";
     tectonic_pdf_io = relocateManifestSubdir "pdf_io";
   };
 
-
-  labeler = config.languages.rust.import (lib.cleanSource ./.) { inherit crateOverrides; };
+  labelerSrc = fileset.toSource {
+    root = ./.;
+    fileset = fileset.intersection
+      (fileset.fromSource (lib.sources.cleanSource ./.))
+      (fileset.fileFilter (
+        file:
+        !(
+          file.hasExt "json"
+          || file.hasExt "md"
+          || file.hasExt "sh"
+          || file.hasExt "png"
+          || hasPrefix "." file.name
+          || hasPrefix "Cloudron" file.name
+          || hasPrefix "devenv" file.name
+          || hasPrefix "Docker" file.name
+        )
+      ) ./.);
+  };
+  
+  labeler = config.languages.rust.import labelerSrc { inherit crateOverrides; };
 in
 {
-  cachix.pull = ["zeroecks-labeler"];
+  cachix.pull = [ "zeroecks-labeler" ];
 
   packages = with pkgs; [ bacon ] ++ rustBuildDeps;
 
   languages.rust.enable = true;
-
 
   enterTest = ''
     echo "Running tests"
     cargo test
   '';
 
-  git-hooks.hooks= {
+  git-hooks.hooks = {
     shellcheck.enable = true;
     clippy.enable = true;
   };
@@ -96,15 +148,23 @@ in
     default = labeler;
   };
 
-  containers.builder = {
-    # This is the nix container, we will then copy from it into the cloudron container
-    name = "labeler-builder";
+  env.SECRETSPEC_PROFILE = lib.mkIf (config.container.isBuilding) "default";
+  env.SECRETSPEC_PROVIDER = lib.mkIf (config.container.isBuilding) "env";
+
+  containers.labeler = {
+    name = "labeler";
+    version = "nightly";
+    startupCommand = "${labeler}/bin/labeler";
+    entrypoint = [ "${labeler}/bin/labeler" ];
     copyToRoot = pkgs.buildEnv {
-      name = "labeler-root";
+      name = "labeler-env";
       paths = [ labeler ];
       pathsToLink = [ "/bin" ];
     };
-    startupCommand = "${labeler}/bin/labeler";
-    entrypoint = [ "${labeler}/bin/labeler" ];
+    fromImage = inputs.nix2container.packages.${pkgs.stdenv.system}.nix2container.pullImage {
+      imageName = "docker.io/cloudron/base";
+      imageDigest = "sha256:1c0666c9abe9e2090d33686826d4e97769b799124573118d41e0d7485135748e";
+      sha256 = "sha256-mtlTI0S0t9nWZ68gKMl5ztLImN1bv6UGW9mV3YaAY/0=";
+    };
   };
 }
