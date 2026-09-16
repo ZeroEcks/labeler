89c7cd1a4fad6cc809c8cd91999486106b49657b
Melody Kelly <melody.kelly@qut.edu.au>
2026-09-16 16:58:41 +1000

    fix: lets just use linux

--- flake.nix
+++ flake.nix
@@ -39,9 +39,6 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
       ];
 
       imports = [
